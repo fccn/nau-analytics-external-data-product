6cf350eea73e2ea277a37971f68942cf907ff207
Vitor Souza Piña <vitor.pina@glintt.com>
2025-11-17 15:14:28 +0000

    adding the base image to improve the build time

---
+++ src/misc/hello_spark.py
@@ -0,0 +1,15 @@
+from pyspark.sql import SparkSession #type: ignore
+
+def main():
+    spark = SparkSession.builder.appName("hello_spark_job").getOrCreate()
+
+    data = [("Madalena", 1), ("Vitor", 2), ("Beatriz", 3)]
+    df = spark.createDataFrame(data, ["name", "value"])
+
+    print("### Hello from Spark on Kubernetes via Airflow ###")
+    df.show(truncate=False)
+
+    spark.stop()
+
+if __name__ == "__main__":
+    main()
