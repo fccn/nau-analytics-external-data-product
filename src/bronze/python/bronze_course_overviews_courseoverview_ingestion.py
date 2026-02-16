0e8838827d78f6a2e1d34039487abfca3e3d117d
Vitor Souza Piña <vitor.pina@glintt.com>
2026-02-16 15:04:35 +0000

    WIP: dividing ingestion into different scripts

--- src/bronze/python/bronze_course_overviews_courseoverview_ingestion.py
+++ src/bronze/python/bronze_course_overviews_courseoverview_ingestion.py
@@ -36,12 +36,14 @@ SELECT
     ) AS T1
     """)
     current_timestamp = spark.sql("SELECT current_timestamp() as c").first()["c"]
+    logging.info(f"executing query in db {query}")
     src_df = read_data_from_sql(spark_session=spark,query=query,jdbc_url=jdbc_url,MYSQL_USER=MYSQL_USER,MYSQL_SECRET=MYSQL_SECRET)
     df = add_ingestion_metadata_column(df=src_df,table=table,current_timestamp=current_timestamp)
     saveTable = f"bronze_local.entidades.{table}"
     df.write.format("iceberg").mode("append").saveAsTable(saveTable)
     scr_full_df = read_data_from_sql(spark_session=spark,query=table,jdbc_url=jdbc_url,MYSQL_USER=MYSQL_USER,MYSQL_SECRET=MYSQL_SECRET)
     nr = validate_ingestion_values(spark_session=spark,src_table_df=scr_full_df,table_name=table)
+    logging.info(f"number of record in table {nr}")
     update_ctrl_table(spark_session=spark,table_name=table,current_timestamp=current_timestamp,number_of_records=nr)
 if __name__ == "__main__":
     main()
