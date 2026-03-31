a246c9c411e63826bbdfd06e0df499778242c18d
paulormonteiro <paulo.r.monteiro@glinttglobal.com>
2026-03-31 18:33:08 +0100

    Fixed utils function import in silver

--- src/silver/silver_auth_user.py
+++ src/silver/silver_auth_user.py
@@ -1,7 +1,7 @@
 from pyspark.sql import DataFrame #type:ignore
 import pyspark.sql.functions as F #type:ignore
 from nau_analytics_data_product_utils_lib import start_iceberg_session,get_required_env #type: ignore
-from silver.utils.silver_utils_functions import update_ctrl_table,get_max_timestamp_for_table
+from utils.silver_utils_functions import update_ctrl_table,get_max_timestamp_for_table
 import logging
 
 logging.basicConfig(
